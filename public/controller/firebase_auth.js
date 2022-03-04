66922e25fc66364e5a38b5835c7200c56d861280
Joseph Emeka <jemeka@uco.edu>
2022-03-04 10:34:22 -0600

    menus added

---
+++ public/controller/firebase_auth.js
@@ -0,0 +1,74 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js"
+
+import * as Elements from '../viewpage/elements.js'
+
+import { DEV } from "../model/constants.js";
+
+import * as Util from '../viewpage/util.js'
+
+const auth = getAuth();
+export let currentUser = null;
+
+export function addEventListeners() {
+
+  onAuthStateChanged(auth, authStateChanged);
+
+  Elements.formSignIn.addEventListener('submit', async e => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const button = e.target.getElementsByTagName('button')[0];
+    const label = Util.disableButton(button);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      Elements.modalSignin.hide();
+    } catch (e) {
+      if (DEV) console.log(e)
+      Util.info('Sign in error', JSON.stringify(e), Elements.modalSignin);
+    }
+    Util.enableButton(button, label);
+  });
+
+  Elements.MENU.SignOut.addEventListener('click', async() => {
+    try {
+      await signOut(auth)
+    } catch (e) {
+      if (DEV) console.log(e);
+      Util.info('Sign out error', JSON.stringify(e));
+    }
+  });
+}
+
+async function authStateChanged(user) {
+
+  currentUser = user;
+
+  if (user) {
+    let menus = document.getElementsByClassName('modal-preauth');
+    for (let i = 0; i < menus.length; i++) {
+      menus[i].style.display = 'none'
+    }
+    menus = document.getElementsByClassName('modal-postauth');
+    for (let i = 0; i < menus.length; i++) {
+      menus[i].style.display = 'block'
+    }
+    console.log('=== signed in')
+  } else {
+    let menus = document.getElementsByClassName('modal-preauth');
+    for (let i = 0; i < menus.length; i++) {
+      menus[i].style.display = 'block'
+    }
+    menus = document.getElementsByClassName('modal-postauth');
+    for (let i = 0; i < menus.length; i++) {
+      menus[i].style.display = 'none'
+    }
+    console.log('=== signed out')
+  }
+
+
+}
---
+++ public/viewpage/elements.js
@@ -0,0 +1,18 @@
+export const MENU = {
+  SignIn: document.getElementById('menu-signin'),
+  Home: document.getElementById('menu-home'),
+  Purchases: document.getElementById('meu-purchases'),
+  SignOut: document.getElementById('menu-signout'),
+  Cart: document.getElementById('menu-cart'),
+  Profile: document.getElementById('menu-profile'),
+}
+
+export const formSignIn = document.getElementById('form-signin');
+
+export const modalInfobox = {
+  modal: new bootstrap.Modal(document.getElementById('modal-infobox'), { backdrop: 'static' }),
+  title: document.getElementById('modal-infobox-title'),
+  body: document.getElementById('modal-infobox-body')
+}
+
+export const modalSignin = new bootstrap.Modal(document.getElementById('modal-signin-form'), { backdrop: 'static' });
